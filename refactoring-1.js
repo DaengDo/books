23d0a53e1a0d47045fe3325071b82af8b4dd0674
DaengDo <ring_yoonsuk@naver.com>
2022-10-11 06:49:01 +0900

    totalAmount 변수를 모듈화 하기위한 준비 작업
    - 반복문 쪼개기로 totalAmount 처리 부분을 따로 처리합니다.
    - 문장 슬라이드하기로 totalAmount 처리 부분을 모아줍니다.

--- refactoring-1.js
+++ refactoring-1.js
@@ -53,7 +53,6 @@ export function statement(invoice, plays) {
     return result;
   }
 
-  let totalAmount = 0;
   let result = `청구 내역(고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
@@ -61,6 +60,10 @@ export function statement(invoice, plays) {
     result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
+  }
+
+  let totalAmount = 0;
+  for (let perf of invoice.performances) {
     totalAmount += amountFor(perf);
   }
 
