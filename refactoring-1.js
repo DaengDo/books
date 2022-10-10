a97395772a591410f5cd4484a4e4c80343943358
DaengDo <ring_yoonsuk@naver.com>
2022-10-11 06:47:02 +0900

    변수 인라인하기로 volumeCredits 변수를 제거

--- refactoring-1.js
+++ refactoring-1.js
@@ -45,12 +45,12 @@ export function statement(invoice, plays) {
   }
 
   function totalVolumeCredits() {
-    let volumeCredits = 0;
+    let result = 0;
     for (let perf of invoice.performances) {
-      volumeCredits += volumeCreditsFor(perf);
+      result += volumeCreditsFor(perf);
     }
 
-    return volumeCredits;
+    return result;
   }
 
   let totalAmount = 0;
@@ -64,9 +64,7 @@ export function statement(invoice, plays) {
     totalAmount += amountFor(perf);
   }
 
-  let volumeCredits = totalVolumeCredits();
-
   result += `총액: ${usd(totalAmount)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 }
