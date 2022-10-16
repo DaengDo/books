2ad8da078e412680e54b7a05376afc96730c8914
DaengDo <ring_yoonsuk@naver.com>
2022-10-16 23:07:25 +0900

    invoice 데이터를 statement에서 전달(가공)
     더 이상 출력 함수에 직접적으로 전달할 필요가 없으므로,
    사용하지 않는 renderPlainText() 매개변수 invoice를 삭제합니다.

--- refactoring-1.js
+++ refactoring-1.js
@@ -1,10 +1,12 @@
 export function statement(invoice, plays) {
   const statementData = {};
+  statementData.customer = invoice.customer;
+  statementData.performances = invoice.performances;
 
-  return renderPlainText(statementData, invoice, plays);
+  return renderPlainText(statementData, plays);
 }
 
-function renderPlainText(data, invoice, plays) {
+function renderPlainText(data, plays) {
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
@@ -52,7 +54,7 @@ function renderPlainText(data, invoice, plays) {
 
   function totalVolumeCredits() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += volumeCreditsFor(perf);
     }
 
@@ -61,16 +63,16 @@ function renderPlainText(data, invoice, plays) {
 
   function getTotalAmount() {
     let totalAmount = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       totalAmount += amountFor(perf);
     }
 
     return totalAmount;
   }
 
-  let result = `청구 내역(고객명: ${invoice.customer})\n`;
+  let result = `청구 내역(고객명: ${data.customer})\n`;
 
-  for (let perf of invoice.performances) {
+  for (let perf of data.performances) {
     result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
